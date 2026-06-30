8ca0262d559571347314989d80b72f7704164937
hamasaki-kotaro <k.hamasaki@tbc-inc.co.jp>
2026-06-30 16:59:27 +0900

    修正

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route; // 【追記】Routeファサードを使えるようにします
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,21 +11,19 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            // shiftcorrection.php を追加で読み込む
-            \Illuminate\Support\Facades\Route::middleware('web')
+            // shiftcorrection.php を追加
+            Route::middleware('web')
                 ->group(base_path('routes/shiftcorrection.php'));
-        },
 
-        // 【追記】ここから
-        then: function () {
+            // dashboard.php を追加
             Route::middleware('web')
                 ->group(base_path('routes/dashboard.php'));
         },
-        // 【追記】ここまで
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
