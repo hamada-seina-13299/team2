3102616591e0503a1df3c04e92da6c49e82c8aab
Shion Nagamine <715-student014@la-study.com>
2026-07-07 17:20:01 +0900

    出勤データの出力

--- routes/report.php
+++ routes/report.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
-
+    Route::get('/reports/attendance', [ReportController::class, 'attendance'])->name('report.attendance');
     Route::get('/reports/shift-approvals', [ShiftApprovalController::class, 'index'])->name('shift.approvals.index');
     Route::post('/reports/shift-approvals/{shiftSubmission}/approve', [ShiftApprovalController::class, 'approve'])->name('shift.approvals.approve');
     Route::post('/reports/shift-approvals/{shiftSubmission}/withdraw', [ShiftApprovalController::class, 'withdraw'])->name('shift.approvals.withdraw');
